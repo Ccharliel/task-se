67f5143517c215d4b013c6a2889e61576fbb1889
Charlie <2321322144@qq.com>
2026-04-24 14:45:59 +0800

    add (task-se) in config print

--- tasks_se/core/config.py
+++ tasks_se/core/config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 CHROME_VERSION = os.getenv("CHROME_VERSION")
-print(f"CHROME_VERSION: {CHROME_VERSION}")
+print(f"CHROME_VERSION (task-se): {CHROME_VERSION}")
 
 CONFIG_PATH = Path(__file__).resolve()
 CORE_DIR = CONFIG_PATH.parent
 LOG_DIR = os.path.join(CORE_DIR.parent, "logs")
-print(f"LOG_DIR: {LOG_DIR}")
+print(f"LOG_DIR (task-se): {LOG_DIR}")
 
